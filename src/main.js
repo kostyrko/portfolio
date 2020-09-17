ca8f5e9f6cf095ce0015cbb5b67e20b734f4d36a
kostyrko <mkostyrko@gmail.com>
2020-09-17 15:22:19 +0200

    Add second lang option

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
+import polish from './translations/pl.js'
+const [pl] = polish
+const dataReload = document.querySelectorAll('[data-reload]')
+
+if (window.location.hash) {
+  console.log('has hash');
+  console.log(window.location.hash);
+  if (window.location.hash === "#pl") {
+    const idArr = Object.keys(pl)
+    idArr.forEach(elem => {
+      document.getElementById(elem).innerHTML = pl[elem]
+    })
+  }
+}
+
+dataReload.forEach(function (elem) {
+  elem.addEventListener('click', function(){
+    console.log(window.location.hash);
+    window.location.hash = elem.hash
+    // console.log(elem);
+    location.reload()
+    return false
+  })
+})
+
+
+
 // ! jQuery
 $('a[href^="#"]').on('click', function(event) {
   // $('#toggle').checked = false;
