b499a1613ea713ff5990c1d92639b22599fec55e
Mikołaj Kostyrko <mkostyrko@gmail.com>
2021-07-30 18:21:01 +0200

    Update content
    Update skill set

--- src/translations/pl.js
+++ src/translations/pl.js
@@ -9,9 +9,9 @@ export default [
       "Skupiam się na co sprawia mi przyjemność i przynosi satysfakcję - nauce nowych oraz rozwijaniu posiadanych przeze mnie umiejętności.",
     key_skills__h2: "Kluczowe umiejętności",
     key_skills__h3_tools: "Narzędzia",
-    key_skills__h3_knowledge: "Znajomość (niewielkie doświadczenie)",
+    key_skills__h3_knowledge: "Znajomość (podstawowe doświadczenie)",
     key_skills__h3_softSkills:
-      "Znajomość oraz niekomercyjne doświadczenie pracy w metodologii oraz narzędziami",
+      "Znajomość oraz doświadczenie pracy w metodologii oraz narzędziami",
       donate_charity_title :'',
       project_donate_charity: 'Koncept strony, która pozwala na przekazywanie pomocy materialnej organizacjom pozarządowym. Kompozycja strony oraz jej stylistyka jest częścią/własnością CodersLab (Portfolio Lab)',
     flacky_meble_title: `<span class="code">&lt;</span>
@@ -36,7 +36,7 @@ export default [
     about__info_1:
       "Naukę programowania rozpocząłem pod koniec 2018 roku od Pythona, wówczas pracowałem jako adiunkt w zakładzie Cyfrowej Geoarcheologii na Uniwersytecie w Bambergu (Niemcy). Planowałem umiejętność programowania powiązać z analizą przestrzennych danych w ujęciu chronologicznym.",
     about__info_2:
-      "Z czasem jednak moje zainteresowania skierowały się w stronę programowania Webowego, po krótkiej przygodzie z Back-endem postanowiłem skupić się Front-endzie. Na początku 2020 roku zdecydowałem się na zmianę dotychczasowej profesji i poświęcić swój czas w pełni na naukę programowania.",
+      "Z czasem jednak moje zainteresowania skierowały się w stronę programowania Webowego, po krótkiej przygodzie z Back-endem postanowiłem skupić się Front-endzie. Na początku 2020 roku zdecydowałem się na zmianę dotychczasowej profesji i poświęcić swój czas w pełni na naukę programowania. Od początku 2021 roku pracuje jako front-end developer.",
     about__info_3: `<em>Dlaczego zdecydowałem się na programowania jako dalszą ścieżkę rozwoju własnych umiejętności? </em> Kreatywność, rozwiązywanie problemów oraz stała potrzeba rozwoju/aktualizowania wiedzy - to są trzy rzeczy, które cenię i którymi chciałbym stykać się w pracy na co dzień.`,
     about__info_4: `Od początku zdobywam doświadczenie poprzez systematyczną naukę realizowaną we własnym zakresie oraz biorąc udział w różnego rodzaju
   kursach <span>( np. CodersLab - JavaScript Developer (<a href="https://coderslab.user.com/media/uploads/lduvtk-coderslab/szczegolowy-program-kursu-javascript-developer-17-06-2020-e257aae58a3b.pdf" target="_blank">program kursu</a>), patrz również <a href="https://www.udemy.com/user/mkostyrko/" target="_blank"> mój profil na Udemy</a> )</span>.`,
